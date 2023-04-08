fbb69bff511496af1985f2b76f20cea53ff25f37
Andrew Kaminer <andrewkaminer@gmail.com>
2023-04-08 16:48:44 -0400

    Added functions 'preprocess_video' and 'preprocess_image' to preprocessing/image_input

---
+++ openCIP/preprocessing/image_input.py
@@ -0,0 +1,54 @@
+'''Functions for preprocessing a video'''
+
+import cv2 as cv
+import numpy as np
+
+
+def preprocess_video(filename: str):
+    '''
+    Preprocesses the video and writes the new, preprocessed, video to a file.
+    Parameters:
+        filename: str - The filename of the video to be preprocessed
+    Returns:
+        NEW_FILENAME: str - The filename of the new, preprocessed, video.
+    '''
+
+    NEW_FILENAME = "preprocessed.mp4"
+    VIDEO_CODEC = "MP4V"
+
+    video = cv.VideoCapture(filename)
+    fps = video.get(cv.cv2.CAP_PROP_FPS)
+    width = video.get(cv.cv2.CAP_PROP_WIDTH)
+    height = video.get(cv.cv2.CAP_PROP_HEIGHT)
+
+    out = cv.VideoWriter(NEW_FILENAME,
+                         cv.VideoWriter_fourcc(*VIDEO_CODEC),
+                         fps,
+                         (width, height)
+                         )
+
+    framecount = int(video.get(cv.cv2.CAP_PROP_FRAME_COUNT))
+
+    for frame in range(framecount):
+        ret, img = video.read()
+
+        if not ret:
+            break
+
+        img = preprocess_image(img)
+        out.write(img)
+
+    video.release()
+    out.release()
+    return NEW_FILENAME
+
+
+def preprocess_image(img: np.array) -> np.array:
+    '''
+    Takes an image of the video from preprocess_video and does some sort of preprocessing on it.
+    Parameters:
+        img: np.array - The image to be processed
+    Returns:
+        img: np.array - The new, processed, image
+    '''
+    return img
