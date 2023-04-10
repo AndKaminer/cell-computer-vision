71d1b329a383d98441d1242df7118fc1291b7429
Andrew Kaminer <andrewkaminer@gmail.com>
2023-04-10 17:43:02 -0400

    Figure out a bit of how to do preprocessing. See preprocessing module

--- openCIP/utils/preprocess_choice.py
+++ openCIP/utils/preprocess_choice.py
@@ -7,4 +7,34 @@ def preprocess_choice() -> int:
     '''
     Manages choosing and previewing different preprocessing for the video.
     '''
+    if Config.GUI:
+        return preprocess_choice_GUI()
+    else:
+        return preprocess_choice_CLI()
+
+
+def preprocess_choice_GUI():
+    '''
+    Provides an interface for choosing and previewing different preprocessing
+    types in GUI mode.
+    '''
     pass
+
+
+def preprocess_choice_CLI():
+    '''
+    Provides an interface for choosing and previewing different preprocessing
+    types in CLI mode.
+    '''
+    pass
+
+    # IMAGE GRADIENTS/FILTERS TO IMPLEMENT:
+    # Sobel
+    # Scharr
+    # Laplacian
+    # Gauss
+    # Brightness up
+    # Brightness down
+    # Contrast up
+    # Contrast down
+    # Canny
